0b48140432cc9ee2ebd37b5be1f578f691e02a2f
m-abbas-13 <abbas.m@goodbusinesslab.org>
2026-02-20 01:18:29 +0530

    fix typo

--- index.ts
+++ index.ts
@@ -71,7 +71,7 @@ app.get("health", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/dist")));
 
-  app.get("/{*splat", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "client/dist/index.html"));
   });
 }
